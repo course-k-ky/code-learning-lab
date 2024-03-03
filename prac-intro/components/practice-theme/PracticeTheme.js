a52153e6283cb5f547306aa123cf364f0f6f5381
practice_user <practice@mail.co.jp>
2024-03-04 08:44:17 +0900

    テーマとストーリーブック

--- prac-intro/components/practice-theme/PracticeTheme.js
+++ prac-intro/components/practice-theme/PracticeTheme.js
@@ -1,14 +1,14 @@
 import { Button, CssBaseline, ThemeProvider } from "@mui/material";
-import generateTheme from "./GenerateTheme";
+import generateTheme from "./generateTheme";
 import Content from "./Content";
 import { useState } from "react";
 
 const PracticeTheme = () => {
     const [mode, setMode] = useState('light');
-
+    const theme = generateTheme;
     return (
         <>
-            <ThemeProvider theme={mode === 'light' ? 'ダーク' : 'ライト'}>
+            <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <Button variant="outlined"
                     onClick={() => setMode(prev => prev === 'light' ? 'dark' : 'light')}>
